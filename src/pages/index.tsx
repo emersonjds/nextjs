87494b6a3d50f52110768f73fb7756da73b8f108
Emerson Silva <emerson_jdss@hotmail.com>
2021-03-11 22:59:43 -0300

    create a simulate server to get data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,33 @@
+import { useState, useEffect } from "react";
+
 import { Title } from "../styles/pages/Home";
 
+interface IHouse {
+  id: number;
+  owner: string;
+  houseName: string;
+  value: number;
+}
+
 export default function Home() {
+  const [houses, setHouses] = useState<IHouse[]>([]);
+
+  useEffect(() => {
+    //num fetch o response sempre tem que ser transformado em json
+    fetch("http://localhost:3333/houses").then((response) => {
+      response.json().then((data) => {
+        setHouses(data);
+      });
+    });
+  }, []);
   return (
     <div>
       <Title>Home Next</Title>
+      <ul>
+        {houses.map((house) => (
+          <li key={house.id}>{house.houseName}</li>
+        ))}
+      </ul>
     </div>
   );
 }
