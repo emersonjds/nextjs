fe6096ca74fac8f9ee5e092d1ce5f1402ae5bea6
Emerson Silva <emerson_jdss@hotmail.com>
2021-03-18 21:43:00 -0300

    config json-server

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 import { Title, TextProduct } from "../styles/pages/Home";
 
@@ -26,7 +26,7 @@ export default function Home() {
       <Title>Home Next</Title>
       <ul>
         {houses.map((house) => (
-          <TextProduct key={house.id}>{house.houseName}</TextProduct>
+          <TextProduct key={house.id}>{house.owner}</TextProduct>
         ))}
       </ul>
     </div>
