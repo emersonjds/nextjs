ee037aa482d797e0e2ba60a67aca0e4843660604
Emerson Silva <emerson_jdss@hotmail.com>
2021-03-20 23:05:43 -0300

    config products page

--- src/pages/products/top10.tsx
+++ src/pages/products/top10.tsx
@@ -2,45 +2,38 @@ import { GetStaticProps } from "next";
 
 import { Title, TextProduct } from "../../styles/pages/Home";
 
-
 interface IProduct {
-    id: number;
-    productName: string;
-    price: number;
+  id: number;
+  productName: string;
+  price: number;
 }
 
 interface ProductProps {
-    products: IProduct[]
+  products: IProduct[];
 }
 
 export default function Top10({ products }: ProductProps) {
-    return (
-        <div style={{padding: '2em'}}>
-            <ul>
-                <Title>Produtos</Title>
-                {
-                    products.map(product => (
-                        <li key={product.id}>
-                            <TextProduct>
-                                {product.productName}
-                            </TextProduct>
-                        </li>
-                    ))
-                }
-            </ul>
-        </div>
-    )
+  return (
+    <div style={{ padding: "2em" }}>
+      <ul>
+        <Title>Produtos</Title>
+        {products.map((product) => (
+          <li key={product.id}>
+            <TextProduct>{product.productName}</TextProduct>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
-
 
 export const getStaticProps: GetStaticProps<ProductProps> = async (context) => {
-    const response = await fetch('http://localhost:3333/products')
-    const products = await response.json()
+  const response = await fetch("http://localhost:3333/products");
+  const products = await response.json();
 
-    return {
-        props: {
-            products
-        }
-    }
-}
-
+  return {
+    props: {
+      products,
+    },
+  };
+};
